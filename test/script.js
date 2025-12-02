d77a4b84c7eb1dd3f33592163516d0b00f7a9b75
Boss <khunboss2252@gmail.com>
2025-12-03 00:32:32 +0700

    a

--- test/script.js
+++
@@ -1,6 +0,0 @@
-const name = prompt('What is your name')
-const acceptTerm = confirm('Do you accept the terms?')
-
-const result = `Hello ${name}, you accepted the terms? ${acceptTerm}`
-alert(result)
-console.log(result)
